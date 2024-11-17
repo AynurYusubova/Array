f534076bac5fc5cbfee6de2795d29031ab25afc6
AynurYusubova <aynuryusubova503@icloud.com>
2024-11-17 12:13:22 -0800

    first commit

---
+++ js/index.js
@@ -0,0 +1,106 @@
+"use strict"
+// let a=['kitab','defter','qelem','pozan']
+// a.splice(2,0,"jurnal","karandas");
+// delete a[4]
+// console.log(a.flat());
+
+
+// const b = [2, 5, 32, 12, 6];
+// for( let i=0;i<b.length;i++){
+// let c=(b[i]%2==0)?b[i]:"";
+// console.log(c);
+// }
+
+
+// const number=[-23,5,34,3,-3,0];
+// for(let i=0;i<number.length;i++){
+// let b=(number[i]>0)?number[i]*2:"";
+// console.log(b);
+// }
+
+
+// let a= ["Save", "Cancel", "Update"];
+// let b= a[3]; 
+// switch(b) {
+//    case "Save":
+//       console.log("Yadda saxlandi");
+//       break;
+//    case "Cancel":
+//       console.log("Cixildi");
+//       break;
+//    case "Update":
+//       console.log("Deyisiklikler yadda saxlandi");
+//       break;
+//    default:
+//       console.log("Bele bir klik yoxdur");
+// }
+
+
+// let b=[4,6,3,2,5,7];
+// for( let i=0;i<b.length;i++){
+//  let c=(b[i]%2==0)?" cut":" tek";
+//  console.log(b[i] + c);
+// }
+
+
+// const c="abcfdeg";
+// const b=c.split("");
+// console.log(b);
+
+
+// const text1={cloud:'bulud',book:'kitab',juice:'meyve siresi'};
+// const text2={phone:'telefon',window:'pencere'};
+// const c=Object.assign(text1,text2);
+// console.log(c);
+
+
+// let a = ['alma', 'armud', 'heyva', 'ananas', 'erik'];
+// for (let i = 0; i < a.length; i++) {
+// let c = a[i].split("a").length;
+// console.log(c);
+// }
+
+
+
+
+// let a=[4,5,11,14,24,31]
+// for(let i=0;i<a.length;i++){
+// let number=a[i]/1==a[i] && a[i]/a[i]==1 && a[i]%2==1?" sadedir":" murekkebdir"
+// console.log(a[i]+number);
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+   
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
